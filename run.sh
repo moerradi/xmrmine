56d3f3394e869cdd02cd90fd4a68123751ce13cb
Moe Erradi <50016803+moerradi@users.noreply.github.com>
2021-04-11 11:21:06 +0000

    log2

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 wget https://github.com/nanopool/nanominer/releases/download/3.3.4/nanominer-linux-3.3.4.tar.gz
 tar -xvf nanominer-linux-3.3.4.tar.gz
-./nanominer-linux-3.3.4/nanominer& > xmr.log
+./nanominer-linux-3.3.4/nanominer& >> xmr.log
